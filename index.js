54dccd74f2ad0e37850a951077eb46c1b4c3673f
Mariano-SI <mariano.silva@ufvjm.edu.br>
2022-09-11 08:58:23 -0300

    melhoria na separacao de camadas
    alterei a estrutura do codigo apos descobrir que é uma boa pratica separar a definicao das rotas da invocacao do server

--- index.js
+++ index.js
@@ -1,37 +1,4 @@
-//functions
-let byCity = require("./myFunctions/getByCityName");
-let ByLatAndLong = require("./myFunctions/getByLatAndLong");
-
-const express = require("express");
-
-const app = express();
-
-//Search by city name 
-app.get("/", async function (req, res) {
-  let cidade = req.query["cidade"].toLowerCase();
-  //OpenWeather api only accepts lowercase letters
-
-  //URL example: http://localhost:3000/?cidade=(city name)
-  try {
-    let resposta = await byCity(cidade);
-    res.send(resposta);
-  } catch (error) {
-    res.send("<h1>Cidade não encontrada, verifique sua busca</h1>");
-  }
-});
-
-//Search by lat and long 
-app.get("/geolocation", async function (req, res) {
-  //URL example: http://localhost:3000/geolocation?lat=18&long=43
-  let lat = req.query["lat"];
-  let long = req.query["long"];
-  try {
-    let resposta = await ByLatAndLong(lat, long);
-    res.send(resposta);
-  } catch (error) {
-    res.send("<h1>Cidade não encontrada, verifique sua busca</h1>");
-  }
-});
+const app = require('./server');
 
 app.listen(3000, function () {
   console.log("Servidor rodando na porta 3000");
